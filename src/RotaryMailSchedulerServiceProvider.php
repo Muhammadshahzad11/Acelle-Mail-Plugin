4b0f3880c7757b3f8ee3bb72cba3b793ff45d36b
shah sawood <shahsawoodshinwari@gmail.com>
2025-05-01 17:14:56 +0500

    🔧 Update README and LICENSE files to reflect recent changes in the repository structure and clarify usage instructions for the Acelle Mailer Extended package.

---
+++ src/RotaryMailSchedulerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Muhammadshahzad\RotaryMailScheduler;
+
+use Illuminate\Support\ServiceProvider;
+
+class RotaryMailSchedulerServiceProvider extends ServiceProvider
+{
+  public function register()
+  {
+    //
+  }
+
+  public function boot()
+  {
+    //
+  }
+}
